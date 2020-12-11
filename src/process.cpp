84ca52d3b408ee1abb5e67571bd84b4e528a22b8
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-11 09:28:11 -0600

    Remove 100 * it's not necessary

--- src/process.cpp
+++ src/process.cpp
@@ -13,7 +13,8 @@ using std::vector;
 
 Process::Process(int pid)  : pid_(pid) {
     user_ = LinuxParser::User(pid);
-    command_ = LinuxParser::Command(pid);
+    string command = LinuxParser::Command(pid);    
+    command_ = (command.length() < 40) ? command : command.substr(0,40) + "...";    
 } 
 
 
@@ -23,7 +24,7 @@ int Process::Pid() { return pid_; }
 float Process::CpuUtilization() { 
     long total_time = LinuxParser::ActiveJiffies(pid_);
     long seconds = UpTime();
-    return 100 * ((float)(total_time / LinuxParser::Jiffies()) / seconds);
+    return ((float)(total_time / LinuxParser::Jiffies()) / seconds);
 }
 
 
